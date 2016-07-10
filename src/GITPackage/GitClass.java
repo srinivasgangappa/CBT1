ce84fc8bf0b3835a0025f5317ca13e1f3b33df89
srinivasgangappa <srinivasgangappa.87@gmail.com>
2016-07-10 14:50:40 +0530

    teasfd

---
+++ src/GITPackage/GitClass.java
@@ -0,0 +1,18 @@
+package GITPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitClass {
+	@Test
+	public void test()
+	{
+		Reporter.log("Test", true);
+	}
+	
+	@Test
+	public void test1()
+	{
+		Reporter.log("Test", true);
+	}
+}
